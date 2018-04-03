92c6b30a09f8ac441e5a228bfddea65b6bbbe05d
mrs.leonenko <mrs.leonenko@gmail.com>
2018-04-03 09:41:29 -0400

    callbacks

--- index.js
+++ index.js
@@ -3,17 +3,43 @@
 //     area: (x,y) => (x*y)
 // }
 
-var rect = require('./rectangle');
+ var rect = require('./rectangle');
 
 function solveRect(l,b) {
-    console.log("Solving for rect with l = " + l + " and b = " + b);
+   // console.log("Solving for rect with l = " + l + " and b = " + b);
 
-    if ( l <= 0 || b <= 0) {
-        console.log("Unexpected input");
-    } else {
-         console.log("The area of rect is " + rect.area(l,b));
-    }
+    rect (l, b, (err, rectangle) => {
+        if (err) {
+                console.log(err.message);
+            }
+        else {
+            console.log(rectangle.area() + " - area and perimeter is " + rectangle.perimeter());
+            //            console.log("The area of the rectangle of dimensions l = "
+            //     + l + " and b = " + b + " is " + rectangle.area());
+            // console.log("The perimeter of the rectangle of dimensions l = "
+            //     + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("sflj");
 }
+
+
+// function solveRect(l,b) {
+//     console.log("Solving for rectangle with l = "
+//                 + l + " and b = " + b);
+//     rect(l,b, (err,rectangle) => {
+//         if (err) {
+// 	        console.log("ERROR: ", err.message);
+// 	    }
+//         else {
+//             console.log("The area of the rectangle of dimensions l = "
+//                 + l + " and b = " + b + " is " + rectangle.area());
+//             console.log("The perimeter of the rectangle of dimensions l = "
+//                 + l + " and b = " + b + " is " + rectangle.perimeter());
+//         }
+//     });
+//     console.log("This statement after the call to rect()");
+// };
 
 solveRect(2,4);
 solveRect(5,3);
